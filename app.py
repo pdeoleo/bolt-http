537d3b61c67c1ce14c74a3730cc3e37505641102
pxdeoleo <pdeoleo@ipexdr.com>
2021-11-28 23:37:14 -0400

    app main file

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from slack_bolt import App
+
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNIN_SECRET")
+)
+
+@app.message("hello")
+def message_hello(message, say):
+    say(f"Hey there <@{message['user']}>!")
+
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
+    
