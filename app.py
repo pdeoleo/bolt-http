53a704da94cfc85e9b5e9ab4c326ad77611c0a31
pxdeoleo <pdeoleo@ipexdr.com>
2021-12-02 17:54:53 -0400

    Working paging from /organizations [perPage] [page]
    Some aesthetic changes to the /organizations block view

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-import os, requests, meraki, json, sys
+import os, pprint
+from os import path
+import requests
+import meraki
+import json
+import sys
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 if sys.version_info[0] == 3 and sys.version_info[1] < 10 or sys.version_info[0] < 3:
-	raise Exception('Requires python 3.10 or higher.')
+    raise Exception('Requires python 3.10 or higher.')
 
 # SOCKET MODE SETUP
 
@@ -17,277 +22,187 @@ app = App(
 #     signing_secret=os.environ.get("SLACK_SIGNIN_SECRET")
 # )
 
+
 def chunks(lst, n):
+    chunked_lst = []
     for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+        chunked_lst.append(lst[i:i + n])
+    return chunked_lst
+
 
 @app.command("/organizations")
-def cmd_organizations(ack, say, command):
+def cmd_organizations(ack, say, command, client):
     ack()
-
-    # match x:
-    #     case 1:
-    #         pass
-    #     case _:
-    #         pass
-
-
-    dashboard = meraki.DashboardAPI(os.environ.get('MERAKI_DASHBOARD_API_KEY'))
-    organizations = chunks(dashboard.organizations.getOrganizations(), 3)
     
-
-    blocks = [
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "Hello, Assistant to the Regional Manager Dwight! *Michael Scott* wants to know where you'd like to take the Paper Company investors to dinner tonight.\n\n *Please select a restaurant:*"
-			}
-		},
-		{
-			"type": "divider"
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "*Organization 2*\nAPI enabled: :white_check_mark:\n URL: <https://www.url.com>"
-			}
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "*Organization 1*\nAPI enabled: :x:\n URL: <https://www.url.com>"
-			}
-		},
-		{
-			"type": "divider"
-		},
-		{
-			"type": "context",
-			"elements": [
-				{
-					"type": "plain_text",
-					"text": "Showing 2 out of x",
-					"emoji": True
-				}
-			]
-		},
-		{
-			"type": "actions",
-			"elements": [
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "First",
-						"emoji": True
-					},
-					"value": "first"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "Previous",
-						"emoji": True
-					},
-					"value": "prev"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "1",
-						"emoji": True
-					},
-					"value": "page_1"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "2",
-						"emoji": True
-					},
-					"value": "page_2"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "3",
-						"emoji": True
-					},
-					"value": "page_3"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "Next",
-						"emoji": True
-					},
-					"value": "next"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "Last",
-						"emoji": True
-					},
-					"value": "last"
-				}
-			]
-		}
-	]
-
-    # for i, org in enumerate(user_orgs):
-    #     user_orgs[i]['networks'] = dashboard.organizations.getOrganizationNetworks(org['id'])
+    msg = say("...")
     
-    # for org in user_orgs:
-    #     msg += f"{org['name']}\n"
-    #     for network in org['networks']:
-    #         msg += f"-{network['name']}\n"
+    split_command = command['text'].split()
+
+    print(split_command)
+
+    match len(split_command):
+        case 1:
+            perPage = int(split_command[0])
+            page = 1
+        case 2:
+            perPage = int(split_command[0])
+            page = int(split_command[1])
+        case _:
+            perPage = 3
+            page = 1
+
+    client.chat_update(
+        ts=msg['ts'], 
+        channel=msg['channel'],
+        text="Connecting to Meraki Dashboard API...")
+    
+    try:
+        dashboard = meraki.DashboardAPI(os.environ.get('MERAKI_DASHBOARD_API_KEY'), log_path='./log_meraki')
+        organizations = dashboard.organizations.getOrganizations()
+    except:
+        client.chat_update(
+            ts=msg['ts'], 
+            channel=msg['channel'],
+            text="Connection failed")
+        return None
+
+    # with meraki.DashboardAPI(os.environ.get('MERAKI_DASHBOARD_API_KEY'), log_path='./log_meraki') as dashboard:
+    #     organizations = dashboard.organizations.getOrganizations()
+
+    organization_chunks = chunks(organizations, perPage)
+
+    block_header = [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"Hello,  *{command['user_name']}*.\n\n *These are your managed organizations:*"
+            }
+        },
+        {
+            "type": "divider"
+        }
+    ]
+
+    block_body_organizations = []
+
+    for org in organization_chunks[page]:
+        block_body_organizations.append(
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"*{org['name']}*\nAPI enabled: {':white_check_mark:' if org['api']['enabled'] else ':x:'}\n URL: <{org['url']}>"
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Details",
+                        "emoji": True
+                    },
+                    "value": f"get_org_{org['id']}",
+                    "action_id": f"get_org_{org['id']}"
+			    }
+            }
+        )
+
+    block_footer = [
+        {
+            "type": "divider"
+        },
+        {
+            "type": "context",
+            "elements": [
+                {
+                    "type": "mrkdwn",
+                    "text": f"Showing *{perPage}* out of *{len(organizations)}* | Page *{page}* of *{len(organization_chunks)}*",
+                }
+            ]
+        }
+    ]
+
+    # Only add navigation buttons if more than one page
+    if len(organization_chunks) > 1:
+        block_nav_buttons = {
+            "type": "actions",
+            "elements": []
+        }
+        
+        # 'First' btn only if not in first page
+        if page != 1:
+            block_nav_buttons['elements'].append(
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "First :black_left_pointing_double_triangle_with_vertical_bar:",
+                        "emoji": True
+                    },
+                    "value": "first"
+                }
+            )   
+
+        # 'Prev' btn only if beyond second page
+        if page > 2:
+            block_nav_buttons['elements'].append(
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Previous :arrow_backward:",
+                        "emoji": True
+                    },
+                    "value": "prev"
+                }
+            )
+        
+        # 'Next' only if before penultimate page
+        if page < len(organization_chunks) - 1:
+            block_nav_buttons['elements'].append(
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Next :arrow_forward:",
+                        "emoji": True
+                    },
+                    "value": "next"
+                }
+            )
+
+        # 'Last' only if not in last page
+        if page != len(organization_chunks):
+            block_nav_buttons['elements'].append({
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Last :black_right_pointing_double_triangle_with_vertical_bar:",
+                    "emoji": True
+                },
+                "value": "last"
+            })
+
+
+        block_pages_buttons = [
+        {
+            "type": "button",
+            "text": {
+                "type": "plain_text",
+                "text": str(n),
+                "emoji": True
+            },
+            "value": f"page_{n}"
+        } for n in range(1, len(organization_chunks)+1) if n != page]
+        
+        block_nav_buttons['elements'].extend(block_pages_buttons)
+        block_footer.append(block_nav_buttons)
 
     say(
-        blocks=blocks,
-        text="x organizations have been found."
-    )
-    
-
-@app.message("organizations")
-def message_organizations(message, say):
-    # Load organizations for APP home page
-
-    # dashboard = meraki.DashboardAPI(os.environ.get('MERAKI_DASHBOARD_API_KEY'))
-    # user_orgs = dashboard.organizations.getOrganizations(perPage=5)
-
-    blocks = [
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "Hello, Assistant to the Regional Manager Dwight! *Michael Scott* wants to know where you'd like to take the Paper Company investors to dinner tonight.\n\n *Please select a restaurant:*"
-			}
-		},
-		{
-			"type": "divider"
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "*Organization 2*\nAPI enabled: :white_check_mark:\n URL: <https://www.url.com>"
-			}
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "*Organization 1*\nAPI enabled: :x:\n URL: <https://www.url.com>"
-			}
-		},
-		{
-			"type": "divider"
-		},
-		{
-			"type": "context",
-			"elements": [
-				{
-					"type": "plain_text",
-					"text": "Showing 2 out of x",
-					"emoji": True
-				}
-			]
-		},
-		{
-			"type": "actions",
-			"elements": [
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "First",
-						"emoji": True
-					},
-					"value": "first"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "Previous",
-						"emoji": True
-					},
-					"value": "prev"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "1",
-						"emoji": True
-					},
-					"value": "page_1"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "2",
-						"emoji": True
-					},
-					"value": "page_2"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "3",
-						"emoji": True
-					},
-					"value": "page_3"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "Next",
-						"emoji": True
-					},
-					"value": "next"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "Last",
-						"emoji": True
-					},
-					"value": "last"
-				}
-			]
-		}
-	]
-
-    # for i, org in enumerate(user_orgs):
-    #     user_orgs[i]['networks'] = dashboard.organizations.getOrganizationNetworks(org['id'])
-    
-    # for org in user_orgs:
-    #     msg += f"{org['name']}\n"
-    #     for network in org['networks']:
-    #         msg += f"-{network['name']}\n"
-
-    say(
-        blocks=blocks
+        blocks=block_header + block_body_organizations + block_footer,
+        text=f"{len(organizations)} organizations have been found."
     )
 
-    # with meraki.DashboardAPI(os.environ.get('MERAKI_DASHBOARD_API_KEY')) as dashboard:
-    #     # meraki_dashboard.
-    #     pass
 
 
 @app.event("app_home_opened")
@@ -348,9 +263,11 @@ def message_hello(message, say):
 
 
 if __name__ == "__main__":
+    if not path.exists('./log_meraki'):
+        os.makedirs('./log_meraki')
+    # SOCKET MODE
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
     
-	# SOCKET MODE
-	SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
 
     # HTTP MODE
     # app.start(port=int(os.environ.get("PORT", 3000)))
